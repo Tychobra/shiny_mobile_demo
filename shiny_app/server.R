6f9ef993a209dfa3d7d612ac1226c36f31bf709c
nickmerl <f.n.merlino@gmail.com>
2019-12-31 16:09:05 -0500

    changed highcharter data to s&p log total return

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,6 +1,13 @@
 function(input, output, session) {
   
+  pe_ratio <- .2
+  
+  
   output$histogram <- renderHighchart(
-    hchart(hist(rnorm(100)))
+    hc <- highchart() %>%
+      hc_add_series(
+        name = "log total return",
+        data = s_p_log_time_series_tr
+      )
   )
 }
