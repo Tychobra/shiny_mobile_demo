65e6badd5e0b01972b16dd8b4f74b52d3b10923c
nickmerl <f.n.merlino@gmail.com>
2020-03-11 18:58:16 -0400

    commented out sidebar content and put pe slider in body of page

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -6,24 +6,36 @@ f7Page(
       theme = "light",
       effect = 'cover',
       f7PanelMenu(
-          f7Slider(
-            "pe_pct_weight",
-            label = "P/E Weights (%)",
-            min = 0,
-            max = 100,
-            value = 20
-          ),
-          br(),
-          f7Block(
-            f7BlockHeader(
-              "What is P/E weight?"
-            ),
-            em(
-              "P/E weight allows you select how much contribution the current P/E and shiller P/E have.
-              For example, a selected weight of 0.2 means that the current P/E is weighted 20% and the shiller PE is weighted 80%"
-            )
-          )
-      )),
+        f7PanelItem(
+          tabName = 'tab_in_sidebar_test',
+          h3("I am a tab")
+          # f7Slider(
+          #   "pe_pct_weight",
+          #   label = "P/E Weights (%)",
+          #   min = 0,
+          #   max = 100,
+          #   value = 20
+          # )
+        )
+          # f7Slider(
+          #   "pe_pct_weight",
+          #   label = "P/E Weights (%)",
+          #   min = 0,
+          #   max = 100,
+          #   value = 20
+          # ),
+          # br(),
+          # f7Block(
+          #   f7BlockHeader(
+          #     "What is P/E weight?"
+          #   ),
+          #   em(
+          #     "P/E weight allows you select how much contribution the current P/E and shiller P/E have.
+          #     For example, a selected weight of 0.2 means that the current P/E is weighted 20% and the shiller PE is weighted 80%"
+          #   )
+          # )
+        )
+      ),
     navbar = f7Navbar(
       title = 'Valuation',
       left_panel = TRUE
@@ -37,6 +49,13 @@ f7Page(
         active = TRUE,
         f7List(
           f7Row(
+            f7Slider(
+              "pe_pct_weight",
+              label = "P/E Weights (%)",
+              min = 0,
+              max = 100,
+              value = 20
+            ),
             f7Picker(
               "t_bill_duration",
               label = "Treasury used as Discount Rate",
@@ -48,7 +67,7 @@ f7Page(
               box(
                 status = "primary",
                 h3(textOutput('current_metric')),
-                title = "Current"
+                title = "Current Value of Nick Metric"
               ),
               side = "center"
             ),
