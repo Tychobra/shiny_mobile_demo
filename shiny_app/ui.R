54114861a880a4abab568d256ac6966bbb7bf955
nickmerl <f.n.merlino@gmail.com>
2020-03-12 10:20:15 -0400

    fixed tab issue

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,24 +1,26 @@
 f7Page(
   title = "S&P Valuation",
   f7TabLayout(
-    panels = f7Panel(
-      inputId = "panel",
-      side = 'left',
-      theme = "light",
-      effect = 'cover',
-      f7PanelMenu(
-        id = "panel_menu",
-        f7PanelItem(
-          tabName = 'tab_in_sidebar_test',
-          h3("I am a tab")
-          # f7Slider(
-          #   "pe_pct_weight",
-          #   label = "P/E Weights (%)",
-          #   min = 0,
-          #   max = 100,
-          #   value = 20
-          # )
-        )
+    panels = tagList(
+      f7Panel(
+        inputId = "panel",
+        side = 'left',
+        theme = "dark",
+        effect = 'cover'
+        # f7PanelMenu(
+        #   id = "panel_menu",
+        #   f7PanelItem(
+        #     tabName = 'tab_in_sidebar_test',
+        #     h3("I am a tab")
+        #     # f7Slider(
+        #     #   "pe_pct_weight",
+        #     #   label = "P/E Weights (%)",
+        #     #   min = 0,
+        #     #   max = 100,
+        #     #   value = 20
+        #     # )
+        #   )
+        # )
           # f7Slider(
           #   "pe_pct_weight",
           #   label = "P/E Weights (%)",
@@ -38,114 +40,116 @@ f7Page(
           # )
         )
       ),
-    navbar = f7Navbar(
-      title = 'Valuation',
-      left_panel = TRUE
-    ),
+      navbar = f7Navbar(
+        title = 'Valuation',
+        left_panel = TRUE,
+        right_panel = FALSE
+      ),
 # UI Chart tab --------------------------------------------
-    f7Tabs(
-      animated = TRUE,
-      id = 'tabs',
-      f7Tab(
-        tabName = 'graph',
-        f7List(
-          f7Row(
-            f7Slider(
-              "pe_pct_weight",
-              label = "P/E Weights (%)",
-              min = 0,
-              max = 100,
-              value = 20
-            ),
-            f7Picker(
-              "t_bill_duration",
-              label = "Treasury used as Discount Rate",
-              choices = discount_slider_df$t_bill_duration,
-              value = "10 year"
-            ),
-            highchartOutput('return_graph'),
-            f7Align(
-              box(
-                status = "primary",
-                h3(textOutput('current_metric')),
-                title = "Current Value of Nick Metric"
+      f7Tabs(
+        animated = TRUE,
+        id = 'tabs',
+        f7Tab(
+          tabName = 'graph',
+          active = TRUE,
+          f7List(
+            f7Row(
+              f7Slider(
+                "pe_pct_weight",
+                label = "P/E Weights (%)",
+                min = 0,
+                max = 100,
+                value = 20
               ),
-              side = "center"
-            ),
-            f7Float(
-              side = "right",
-              f7List(
-                f7checkBox(
-                  'show_avg',
-                  label = "Show Metric Mean"
+              f7Picker(
+                "t_bill_duration",
+                label = "Treasury used as Discount Rate",
+                choices = discount_slider_df$t_bill_duration,
+                value = "10 year"
+              ),
+              highchartOutput('return_graph'),
+              f7Align(
+                box(
+                  status = "primary",
+                  h3(textOutput('current_metric')),
+                  title = "Current Value of Nick Metric"
                 ),
-                f7checkBox(
-                  'show_q1',
-                  label = "Show Metric 25th Percentile"
-                ),
-                f7checkBox(
-                  'show_q3',
-                  label = "Show Metric 75th Percentile"
+                side = "center"
+              ),
+              f7Float(
+                side = "right",
+                f7List(
+                  f7checkBox(
+                    'show_avg',
+                    label = "Show Metric Mean"
+                  ),
+                  f7checkBox(
+                    'show_q1',
+                    label = "Show Metric 25th Percentile"
+                  ),
+                  f7checkBox(
+                    'show_q3',
+                    label = "Show Metric 75th Percentile"
+                  )
                 )
               )
             )
           )
-        )
-      ),
+        ),
 # UI Back-test tab --------------------------------------------
-      f7Tab(
-        tabName = "back_test",
-        f7List(
-          f7Tooltip(
-            f7Slider(
-              'not_buy_point',
-              label = "Not Buy Point",
-              min = 0,
-              max = 7,
-              step = 0.1,
-              value = 2
-            ), 'Balance starts at $0 with a monthly $100 income stream available to invest.
-             Select a Nick Metric value under which you will not invest the income stream.'
-          ),
-          br(),
-          f7Align(
-            side = "center",
-            f7Block(
-              strong = TRUE,
-              inset = TRUE,
-              f7BlockHeader("Ending Balance($100 invested monthly no matter what)"),
-                h1(textOutput('benchmark_end_balance'))
-            )
-          ),
-          f7Align(
-            side = "center",
-            f7Block(
-              strong = TRUE,
-              inset = TRUE,
-              f7BlockHeader("Ending Balance($100 investment posponed under Not Buy Point)"),
-              h1(textOutput('end_balance'))
+        f7Tab(
+          tabName = "back_test",
+          f7List(
+            f7Tooltip(
+              f7Slider(
+                'not_buy_point',
+                label = "Not Buy Point",
+                min = 0,
+                max = 7,
+                step = 0.1,
+                value = 2
+              ), 'Balance starts at $0 with a monthly $100 income stream available to invest.
+               Select a Nick Metric value under which you will not invest the income stream.'
+            ),
+            br(),
+            f7Align(
+              side = "center",
+              f7Block(
+                strong = TRUE,
+                inset = TRUE,
+                f7BlockHeader("Ending Balance($100 invested monthly no matter what)"),
+                  h1(textOutput('benchmark_end_balance'))
+              )
+            ),
+            f7Align(
+              side = "center",
+              f7Block(
+                strong = TRUE,
+                inset = TRUE,
+                f7BlockHeader("Ending Balance($100 investment posponed under Not Buy Point)"),
+                h1(textOutput('end_balance'))
+              )
             )
           )
-        )
-      ),
-      f7Tab(
-        tabName = "list_tab",
-        f7List(
-          f7ListGroup(
-            f7ListItem(
-              'ListItem 1'
-            ),
-            f7ListItem(
-              'ListItem 2'
-            ),
-            f7ListItem(
-              'ListItem 3'
-            ),
-            title = "List Title",
+        ),
+        f7Tab(
+          tabName = "list_tab",
+          f7List(
+            f7ListGroup(
+              f7ListItem(
+                'ListItem 1'
+              ),
+              f7ListItem(
+                'ListItem 2'
+              ),
+              f7ListItem(
+                'ListItem 3'
+              ),
+              title = "List Title",
+            )
           )
         )
       )
     )
   )
-)
 
