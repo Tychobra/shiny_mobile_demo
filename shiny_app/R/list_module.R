db6f3080abbe1e3284e72c042c5f395e29f1e431
nickmerl <f.n.merlino@gmail.com>
2020-03-23 12:21:48 -0400

    added more list elements to the tab is scrollable

--- shiny_app/R/list_module.R
+++ shiny_app/R/list_module.R
@@ -6,13 +6,52 @@ list_module_ui <- function(id)  {
   f7List(
     f7ListGroup(
       f7ListItem(
-        'ListItem 1'
+        'This'
       ),
       f7ListItem(
-        'ListItem 2'
+        'Is'
       ),
       f7ListItem(
-        'ListItem 3'
+        'Where'
+      ),
+      f7ListItem(
+        'The'
+      ),
+      f7ListItem(
+        'List'
+      ),
+      f7ListItem(
+        'Items'
+      ),
+      f7ListItem(
+        'Would'
+      ),
+      f7ListItem(
+        'Go'
+      ),
+      f7ListItem(
+        'Item_1'
+      ),
+      f7ListItem(
+        'Item_2'
+      ),
+      f7ListItem(
+        'Item_3'
+      ),
+      f7ListItem(
+        'Item_4'
+      ),
+      f7ListItem(
+        'Item_5'
+      ),
+      f7ListItem(
+        'Item_6'
+      ),
+      f7ListItem(
+        'Item_7'
+      ),
+      f7ListItem(
+        'Item_8'
       ),
       title = "List Title",
     )
