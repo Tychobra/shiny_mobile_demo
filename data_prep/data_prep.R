2b712ed7ccf7e7c5b83cef76e7112965304c22aa
nickmerl <f.n.merlino@gmail.com>
2020-03-23 11:07:26 -0400

    data sourced from new data source.  old data source would delete the a months woth of data every month that passed which was giving us errors

--- data_prep/data_prep.R
+++ data_prep/data_prep.R
@@ -1,10 +1,11 @@
 library(Quandl)
 library(dplyr)
 library(lubridate)
-library(tidyverse)
 library(quantmod)
 
 
+##this data being pulled has problems the dates don't always pull the most recent month.
+##sometimes also data exist for dates in the future("ooooh spooooky")
 shiller_table_month <- Quandl("MULTPL/SHILLER_PE_RATIO_MONTH")
 pe_table_month <- Quandl("MULTPL/SP500_PE_RATIO_MONTH")
 treasury_table_month <- Quandl("USTREASURY/YIELD", collapse="monthly") 
@@ -104,6 +105,120 @@ saveRDS(
   t_bill_geo_means,
   "shiny_app/data/t_bill_geo_means.RDS"
 )
+
+
+
+library(tibble)
+library(rio)
+
+quantmod::getSymbols.yahoo(
+  "^SP500TR",
+  .GlobalEnv,
+  return.class = 'xts',
+  index.class  = 'Date',
+  from = "1990-01-01",
+  to = Sys.Date(),
+  periodicity = "monthly"
+)
+
+SP500TR <- as.data.frame(SP500TR)
+
+SP500TR <- SP500TR %>%
+  select(SP500TR.Close)
+
+names <- rownames(SP500TR)
+row.names(SP500TR) <- NULL
+
+SP500TR <- cbind(names, SP500TR)
+
+names(SP500TR) <- c("Date", "sp_500_tr")
+
+SP500TR$Date <- as_date(SP500TR$Date)
+
+##Converting for use in log TR
+start_s_p_tr_price <- s_p_tr[1, ]$sp_500_tr
+
+s_p_tr_log <- SP500TR %>%
+  mutate(log_returns = log( s_p_tr$sp_500_tr /  start_s_p_tr_price))
+
+saveRDS(
+  SP500TR,
+  "shiny_app/data/s_p_tr.RDS"
+)
+
+saveRDS(
+  s_p_tr_log,
+  "shiny_app/data/s_p_tr_log.RDS"
+)
+
+
+
+# 
+# ##old
+# # s_p_daily <- quantmod::getSymbols(
+# #   "^GSPC", src = "yahoo",
+# #   auto.assign = FALSE
+# # )
+# 
+# ## new
+# 
+# s_p_daily <- s_p_daily %>%
+#   select(
+#     date,
+#     close = GSPC.Close
+#   ) %>%
+#   filter(date > as.Date(Sys.Date() - 31))
+# 
+# saveRDS(
+#   s_p_daily,
+#   "shiny_app/data/s_p_daily.RDS"
+# )
+# 
+# 
+# ##Fetching S&P total return values
+# s_p_daily_tr <- quantmod::getSymbols(
+#   "^SP500TR", src = "yahoo",
+#   auto.assign = FALSE
+# )
+# 
+# s_p_daily_tr <- as.data.frame(s_p_daily_tr)
+# s_p_daily_tr <- as_tibble(s_p_daily_tr)
+# 
+# s_p_daily_tr$Date <- as.Date(s_p_daily_tr$Date)
+# 
+# saveRDS(
+#   s_p_daily_tr,
+#   "shiny_app/data/s_p_daily_tr.RDS"
+# )
+# 
+# 
+# ##Converting for use in log TR
+# start_s_p_tr_price <- s_p_daily_tr[1, ]$Close
+# 
+# 
+# # filter(date > as.Date(Sys.Date() - (20*365)) )
+# s_p_daily_tr_log <- s_p_daily_tr %>%
+#   mutate(log_returns = log( s_p_daily_tr$Close /  start_s_p_tr_price))
+# 
+# saveRDS(
+#   s_p_daily_tr_log,
+#   "shiny_app/data/s_p_daily_tr_log.RDS"
+# )
+
+
+
+# total_r <- Quandl("YALE/SP_RDSPD", api_key="b4H5zGkcGuqDGi_tGr4H", collapse = "monthly")
+#   
+# install.packages("BatchGetSymbols")
+# library(BatchGetSymbols)
+# 
+# ?BatchGetSymbols
+# sp500_log_tr <- BatchGetSymbols(
+#   tickers = "^SP500TR",
+#   first.date = "1990-01-01",
+#   type.return = "log",
+#   freq.data = "monthly"
+# )
 
 
 
