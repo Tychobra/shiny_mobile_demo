f4f2e4ba2186c7d0a911a7b381b860c7329e1a4e
nickmerl <f.n.merlino@gmail.com>
2020-03-23 11:10:42 -0400

    minor name changes for to adjust for new data which was read in

--- shiny_app/R/chart_module.R
+++ shiny_app/R/chart_module.R
@@ -2,44 +2,42 @@
 chart_module_ui <- function(id) {
   ns <-  NS(id)
   
-    f7List(
-      f7Row(
-        f7Slider(
-          ns("pe_pct_weight"),
-          label = "P/E Weights (%)",
-          min = 0,
-          max = 100,
-          value = 20
-        ),
-        f7Picker(
-          ns("t_bill_duration"),
-          label = "Treasury used as Discount Rate",
-          choices = discount_slider_df$t_bill_duration,
-          value = "10 year"
-        ),
-        highchartOutput(ns('return_graph')),
-        # f7Align(
-          box(
-            status = "primary",
-            h3(textOutput(ns('current_metric')),
-               title = "Current Value of Nick Metric"),
-            side = "center"
-          ),
-        f7Float(
-          side = "right",
-          f7List(
-            f7checkBox(
-              ns('show_avg'),
-             label = "Show Metric Mean"),
-            f7checkBox(
-              ns('show_q1'),
-             label = "Show Metric 25th Percentile"),
-            f7checkBox(
-              ns('show_q3'),
-             label = "Show Metric 75th Percentile")
-          )
+  f7List(
+    f7Row(
+      f7Slider(
+        ns("pe_pct_weight"),
+        label = "P/E Weights (%)",
+        min = 0,
+        max = 100,
+        value = 20
+      ),
+      f7Picker(
+        ns("t_bill_duration"),
+        label = "Treasury used as Discount Rate",
+        choices = discount_slider_df$t_bill_duration,
+        value = "10 year"
+      ),
+      highchartOutput(ns('return_graph')),
+      box(
+        status = "primary",
+        h3(textOutput(ns('current_metric')),
+           title = "Current Value of Nick Metric"),
+        side = "center"
+      ),
+      f7Float(
+        side = "right",
+        f7List(
+          f7checkBox(
+            ns('show_avg'),
+            label = "Show Metric Mean"),
+          f7checkBox(
+            ns('show_q1'),
+            label = "Show Metric 25th Percentile"),
+          f7checkBox(
+            ns('show_q3'),
+            label = "Show Metric 75th Percentile")
         )
-      # )
+      )
     )
   )
 }
@@ -50,26 +48,40 @@ chart_module <- function(input, output, session, metrics_) {
   ns <- session$ns
   
   pe_ratio <- reactive({
+    req(input$pe_pct_weight)
+    
     input$pe_pct_weight / 100
   })
   
   ##reactive to current interest rate of a selected t-bill duration
-  discount_rate_ <- reactive({
+  discount_rate <- reactive({
+    req(input$t_bill_duration)
+    
+    # browser()
+    
     discount_slider_df %>%
       filter(t_bill_duration == input$t_bill_duration) %>%
-      pull(value_current_discount[1])
+      pull(value_current_discount[1]) #%>% 
+      # debounce(1000)
   })
   
-  discount_rate <- discount_rate_ %>% debounce(1000)
+  # discount_rate <- discount_rate_ %>% debounce(1000)
   
   ##reactive to grab geometric mean of a selected t-bill duration
   discount_geo_mean <- reactive({
+    req(input$t_bill_duration)
+    
+    # browser()
+    
     discount_slider_df %>%
       filter(t_bill_duration == input$t_bill_duration) %>%
       pull(value_geo_discount[1])
   })
   
   avg_complete_metric <- reactive({
+    req(pe_ratio(), discount_geo_mean())
+    
+    # browser()
     
     pe_weight_input <- pe_ratio()
     t_duration_geo_selected <- discount_geo_mean()
@@ -77,26 +89,37 @@ chart_module <- function(input, output, session, metrics_) {
     pe_component = (1 / avg_pe_100) * pe_weight_input
     shiller_component = (1 / avg_shiller_100) * (1 - pe_weight_input)
     nick_metric = ( shiller_component + pe_component ) * ( 1 / t_duration_geo_selected )
-    
   })
   
   current_complete_metric <- reactive({
+    req(pe_ratio(), discount_rate())
+    
+    # browser()
     
     pe_weight_input <- pe_ratio()
     most_recent_t_bill_selected <-  discount_rate() 
     
-    pe_component = ( 1 / (current_pe_ratios$pe_current ) ) * pe_weight_input
-    shiller_component = (1 / (current_pe_ratios$pe_shiller_current) ) * (1 - pe_weight_input)
-    nick_metric = (shiller_component + pe_component ) * ( 1 / most_recent_t_bill_selected )
+    pe_component <- ( 1 / (current_pe_ratios$pe_current ) ) * pe_weight_input
+    shiller_component <-  (1 / (current_pe_ratios$pe_shiller_current) ) * (1 - pe_weight_input)
+    nick_metric <- (shiller_component + pe_component ) * ( 1 / most_recent_t_bill_selected )
     
+    nick_metric
   })
   
   output$current_metric <- renderText( {
+    req(current_complete_metric())
+    
+    # browser()
+    
     current <- paste(round(100 * current_complete_metric()[1], digits = 2), "%")
     current
   })
   
   complete_metric_over_time <- reactive({
+
+    req(pe_ratio(), discount_rate())
+    
+    # browser()
     
     pe_weight_input <- pe_ratio()
     most_recent_t_bill_selected <-  discount_rate()
@@ -112,22 +135,28 @@ chart_module <- function(input, output, session, metrics_) {
       ) %>%
       select(date, nick_metric)
     
-    xts::xts(x = metrics_$nick_metric, order.by = metrics_$date)
+    
   })
   
   output$return_graph <- renderHighchart({
-    plot_data <- complete_metric_over_time()
+    req(complete_metric_over_time())
+    
+    # browser()
+    
+    out <- complete_metric_over_time()
     s_p_dat <- s_p_log_time_series_tr
     
-    mean_metric <- mean(complete_metric_over_time()) * 100
+    plot_data <- xts::xts(x = out$nick_metric, order.by = out$date, unique = FALSE)
+
+    mean_metric <- mean(out$nick_metric) * 100
     
-    q1_metric <- unname(quantile(complete_metric_over_time(), probs = 0.25) * 100)
-    q3_metric <- unname(quantile(complete_metric_over_time(), probs = 0.75) * 100)
-    
+    q1_metric <- unname(quantile(out$nick_metric, probs = 0.25) * 100)
+    q3_metric <- unname(quantile(out$nick_metric, probs = 0.75) * 100)
+
     out <-  highchart() %>%
       hc_add_series(
         name = "Total Log Return(S&P)",
-        data = s_p_log_time_series_tr * 100,
+        data = s_p_dat * 100,
         type = "line",
         tooltip = list(
           valueDecimals = 2
@@ -151,11 +180,15 @@ chart_module <- function(input, output, session, metrics_) {
         min = 0
       )
     
+    # browser()
+    
     plot_lines_mean <- list(
       value = mean_metric,
       color = "red",
       label = list(text = paste("Mean", ":", round(mean_metric, 2), "%"))
     )
+    
+    # browser()
     
     plot_lines_q1 <- list(
       value = q1_metric,
@@ -177,6 +210,8 @@ chart_module <- function(input, output, session, metrics_) {
           )
         )
     }
+    
+    # browser()
     
     if(input$show_avg == TRUE & input$show_q1 == TRUE & input$show_q3 == FALSE) {
       out <- out %>% 
@@ -237,14 +272,6 @@ chart_module <- function(input, output, session, metrics_) {
         )
     }
     
-    
     out
   })
 }
-
-
-
-
-
-
-
